406c08fe338182f9be71e65c61261a3054f33362
Zenkku <senja.paakkinen@gmail.com>
2025-11-06 11:56:51 +0200

    seiska

--- todo1/src/screens/App.jsx
+++ todo1/src/screens/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
-import './App.css'
+import { useUser } from '../context/useUser'
+import '../App.css'
 import axios from 'axios'
-import Row from './components/Row'
+import Row from '../components/Row'
 
 const url = "http://localhost:3001"
 
@@ -9,6 +10,7 @@ const url = "http://localhost:3001"
 function App() {
   const [task, setTask] = useState('')
   const [tasks, setTasks] = useState([])
+  const { user } = useUser()
 
 useEffect(() => {
  axios.get(url)
@@ -23,9 +25,10 @@ useEffect(() => {
 
 
 const addTask = () => {
+  const headers = {headers: {Authorization: user.token}}
   const newTask = { description: task }
 
-  axios.post(url + "/create", {task: newTask})
+  axios.post(url + "/create", {task: newTask},headers)
   .then(response => {
     setTasks([...tasks,response.data])
     setTask('')
@@ -37,7 +40,9 @@ const addTask = () => {
 
 
   const deleteTask = (deleted) => {
-    axios.delete(url + "/delete/" + deleted)
+    const headers = {headers: {Authorization: user.token}}
+    console.log(headers)
+    axios.delete(url + "/delete/" + deleted,headers)
       .then(response => {
         setTasks(tasks.filter(item => item.id !== deleted))
     })
